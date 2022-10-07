7ba785f6acfd6e500abe0bb9e173fdf027e6b03e
Eduardo Correia <up201909914@fc.up.pt>
2022-10-07 17:45:16 +0100

    added code from guide 2

--- src/application_layer.c
+++ src/application_layer.c
@@ -1,9 +1,72 @@
 // Application layer protocol implementation
 
 #include "application_layer.h"
+#include "link_layer.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef enum
+{
+    OK,
+    ConnectionError,
+} debugType;
+
+
+LinkLayerRole getRole(const char *role){
+    if(strcmp(role,"tx") == 0)
+        return LlTx;
+    if(strcmp(role,"rx") == 0)
+        return LlRx;
+        
+    printf("Invalid input arguments\n");
+    exit(-1);
+}
+
+LinkLayer getConnectionParams(const char *serialPort, const char *role, int baudRate,int nTries, int timeout, const char *filename){
+    LinkLayer connectionParameters;
+
+    LinkLayerRole appRole = getRole(role);
+    connectionParameters.role = appRole;
+    strcpy(connectionParameters.serialPort,serialPort);
+    connectionParameters.baudRate = baudRate;
+    connectionParameters.nRetransmissions = nTries;
+    connectionParameters.timeout = timeout;
+
+    return connectionParameters;
+}
+
+debugType executeLinkLayer(LinkLayer connectionParamenters){
+    if(llopen(connectionParamenters) == 1){
+        if(connectionParamenters.role == LlRx)
+            printf("Established Connection with transmitter\n");
+        else if(connectionParamenters.role == LlTx)
+            printf("Established Connection with reader\n");
+
+    }
+    else{
+        return(ConnectionError);
+    }
+
+    return OK;
+}
+
 
 void applicationLayer(const char *serialPort, const char *role, int baudRate,
                       int nTries, int timeout, const char *filename)
 {
-    // TODO
+    //Setting connection params from main
+    LinkLayer connectionParameters = getConnectionParams(serialPort,role,baudRate,nTries,timeout,filename);
+
+    //execute linklayer code, with error handling
+    switch (executeLinkLayer(connectionParameters))
+    {
+    case ConnectionError:
+        printf("Could not establish connection with the other program, closing application\n");
+        exit(-1);
+        break;
+    default:
+        break;
+    }
 }
